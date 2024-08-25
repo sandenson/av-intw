139a0a6ac8b194795b43b43e0c1f10aafd2196e1
sandenson <lgabrielsandes@gmail.com>
2024-08-25 19:32:39 -0300

    fix: Redirection function actually works now.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 function goTo(page) {
     switch (page) {
         case 'home':
-            window.location.href = window.location.pathname = '';
+            window.location.pathname = '';
             break;
         case 'signup':
-            window.location.href = window.location.pathname = '/auth/signup';
+            window.location.pathname = '/auth/signup';
             break;
         case 'login':
-            window.location.href = window.location.pathname = '/auth/login';
+            window.location.pathname = '/auth/login';
             break;
         default:
-            window.location.href = window.location.pathname = '';
+            window.location.pathname = '';
             break;
     }
 }
